0dec24071a56aadf3546fbf9a06f9d150c0558ba
Mousumi <dutta.mousumi136@gmail.com>
2024-02-08 13:09:27 -0500

    dot vs []

--- Arrays/object.js
+++ Arrays/object.js
@@ -1,6 +1,12 @@
 let person = {};
 person.name = 'Mohita';
-person.name='Pya'
-delete person.name
+person.name='Pya';
+//delete person.name
 
 console.log(person);
+
+//use dot notation vs. [] notation
+console.log(person.name);
+let propName = "name";
+console.log(person.propName);
+console.log(person[propName]);
