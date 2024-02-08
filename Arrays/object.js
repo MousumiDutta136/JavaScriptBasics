55d28ab48cb332112d41dbc38d57405cb8cf3722
Mousumi <dutta.mousumi136@gmail.com>
2024-02-08 13:26:55 -0500

    ways to get properties

--- Arrays/object.js
+++ Arrays/object.js
@@ -1,6 +1,7 @@
 let person = {};
 person.name = 'Mohita';
 person.name='Pya';
+person.age = 30;
 //delete person.name
 
 console.log(person);
@@ -10,3 +11,19 @@ console.log(person.name);
 let propName = "name";
 console.log(person.propName);
 console.log(person[propName]);
+
+//ways to get the properties of an object
+//1. for..in loop
+for(let element in person){
+  console.log(element + ' : ' +  person[element]);
+}
+//2.Object.keys() & forEach()
+Object.keys(person).forEach((key)=>{
+  console.log(key + ' : ' +  person[key]);
+//console.log(person[key]);
+})
+
+//3.Object.values() & forEach()
+Object.values(person).forEach((val)=>{
+console.log(val);
+})
